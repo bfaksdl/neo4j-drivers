f018e07c01bf1d7fa2aae6f700c46f5c5909f377
Gustorn <gustorn@gmail.com>
2017-04-30 21:57:29 +0200

    Add non-throwing smoke test to ChangeSet's toString method

--- neo4j-reactive-driver-interfaces/src/test/java/neo4j/driver/reactive/ChangeSetTest.java
+++ neo4j-reactive-driver-interfaces/src/test/java/neo4j/driver/reactive/ChangeSetTest.java
@@ -4,12 +4,20 @@ import com.google.common.collect.HashMultiset;
 import com.google.common.collect.Multiset;
 import com.google.common.collect.TreeMultiset;
 
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 import neo4j.driver.reactive.data.ChangeSet;
 
 public class ChangeSetTest {
+	ChangeSet<Integer> emptyChangeSet;
+
+	@Before
+	public void before() {
+		emptyChangeSet = new ChangeSet<>(HashMultiset.create(), HashMultiset.create());
+	}
+
 	@Test
 	public void testConstruction() {
 		// Arrange
@@ -22,5 +30,10 @@ public class ChangeSetTest {
 		// Assert
 		assertEquals(positiveExpected, set.getPositive());
 		assertEquals(negativeExpected, set.getNegative());
+	}
+
+	@Test
+	public void testToStringDoesNotThrow() {
+		emptyChangeSet.toString();
 	}
 }
